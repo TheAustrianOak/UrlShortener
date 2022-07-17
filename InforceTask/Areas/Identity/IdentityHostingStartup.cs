0f2f804d8affd65ff68371c04475c134007b96bf
leon.kyiv.dev@gmail.com <max23.sumy.ua@gmail.com>
2022-07-17 23:44:53 +0300

    Add project files.

---
+++ InforceTask/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using InforceTask.Areas.Identity.Data;
+using InforceTask.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InforceTask.Areas.Identity.IdentityHostingStartup))]
+namespace InforceTask.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthorizationDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthorizationDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                    .AddEntityFrameworkStores<AuthorizationDBContext>();
+            });
+        }
+    }
+}
